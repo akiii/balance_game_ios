d4e19202b4e03809eee9654357f60fd738ce2d61
akiii <akiii@sfc.wide.ad.jp>
2012-08-23 19:19:49 +0900

    Modified support interface orientation.

--- BalanceGame/AppDelegate.m
+++ BalanceGame/AppDelegate.m
@@ -93,7 +93,8 @@
 // Supported orientations: Landscape. Customize it for your own needs
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-	return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+    if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft) return true;
+    return false;
 }
 
 
