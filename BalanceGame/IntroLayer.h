2a95fa3478109a94be928f988eef8b99d110ccb6
akiii <akiii@sfc.wide.ad.jp>
2012-08-23 15:42:06 +0900

    Setuped project.

---
+++ BalanceGame/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  BalanceGame
+//
+//  Created by Akifumi on 2012/08/23.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
