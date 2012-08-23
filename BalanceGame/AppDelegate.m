c4886ed90778f2590b93cc9bd32a364927fdc3cf
akiii <akiii@sfc.wide.ad.jp>
2012-08-23 16:04:59 +0900

    Changed link.

--- BalanceGame/AppDelegate.m
+++ BalanceGame/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "cocos2d.h"
 
 #import "AppDelegate.h"
-#import "IntroLayer.h"
+#import "BGGameScene.h"
 
 @implementation AppController
 
@@ -73,7 +73,7 @@
 	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
 
 	// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-	[director_ pushScene: [IntroLayer scene]]; 
+	[director_ pushScene: [BGGameScene scene]];
 
 	
 	// Create a Navigation Controller with the Director
