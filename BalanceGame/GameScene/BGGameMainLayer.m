3718dd65b91dbbceb09346d1049021e01e2fd315
akiii <akiii@sfc.wide.ad.jp>
2012-08-24 11:23:27 +0900

    Modified layer.

--- BalanceGame/GameScene/BGGameMainLayer.m
+++ BalanceGame/GameScene/BGGameMainLayer.m
@@ -8,29 +8,42 @@
 
 #import "BGGameMainLayer.h"
 
+@interface BGGameMainLayer()
+@property (nonatomic, retain) CCSprite *tower;
+@end
 
 @implementation BGGameMainLayer
-
-- (id)init{
-    if (self = [super init]) {
-        self.isTouchEnabled = YES;
-        self.isAccelerometerEnabled = YES;
-    }
-    return self;
-}
+@synthesize tower;
 
 - (void)onEnter{
     [super onEnter];
     
+    self.isTouchEnabled = YES;
+    self.isAccelerometerEnabled = YES;
+    
     CGSize screenSize = [CCDirector sharedDirector].winSize;
     
-    CCSprite *tower = [CCSprite spriteWithFile:@"tokyo_tower.png"];
-    tower.position = ccp(screenSize.width/2, screenSize.height/2);
-    [self addChild:tower];
+    self.tower = [CCSprite spriteWithFile:@"tokyo_tower.png"];
+    self.tower.position = ccp(screenSize.width/2, screenSize.height/2);
+    [self addChild:self.tower];
+    
+    ccTime moveTime = 1.0;
+    float angle = 15;
+
+    self.tower.anchorPoint = ccp(0, 0);
+    id moveLeft = [CCSequence actions:[CCRotateBy actionWithDuration:moveTime angle:-angle], [CCRotateBy actionWithDuration:moveTime angle:angle], nil];
+
+    [self.tower runAction:moveLeft];
+//    [self.tower runAction:[CCRotateTo actionWithDuration:3.0 angle:30]];
 }
 
 - (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration{
     NSLog(@"x : %f, y : %f, z : %f", acceleration.x, acceleration.y, acceleration.z);
+}
+
+- (void)dealloc{
+    self.tower = nil;
+    [super dealloc];
 }
 
 @end
