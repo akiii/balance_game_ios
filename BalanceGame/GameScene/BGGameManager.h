a51ba1e476541072ff53af4aa788e1acd2a65129
akiii <akiii@sfc.wide.ad.jp>
2012-08-26 15:11:21 +0900

    Added a new game management class.

---
+++ BalanceGame/GameScene/BGGameManager.h
@@ -0,0 +1,16 @@
+//
+//  BGGameManager.h
+//  BalanceGame
+//
+//  Created by Akifumi on 2012/08/26.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface BGGameManager : CCNode {
+    
+}
+
+@end
---
+++ BalanceGame/GameScene/BGGameManager.m
@@ -0,0 +1,14 @@
+//
+//  BGGameManager.m
+//  BalanceGame
+//
+//  Created by Akifumi on 2012/08/26.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGGameManager.h"
+
+
+@implementation BGGameManager
+
+@end
