4a0118a5faf5775519ad4513af797bef8f9bdb8b
XXBlackieXX <blackie.stratocaster@gmail.com>
2012-08-27 14:37:21 +0900

    Modified a link.

--- BalanceGame/AppDelegate.m
+++ BalanceGame/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "cocos2d.h"
 
 #import "AppDelegate.h"
-#import "BGGameScene.h"
+#import "BGTopScene.h"
 
 @implementation AppController
 
@@ -73,7 +73,7 @@
 	[CCTexture2D PVRImagesHavePremultipliedAlpha:YES];
 
 	// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-	[director_ pushScene: [BGGameScene scene]];
+	[director_ pushScene: [BGTopScene scene]];
 
 	
 	// Create a Navigation Controller with the Director
