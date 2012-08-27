11d4ac38a810101fd275777912b3bffa7756f42d
akiii <akiii@sfc.wide.ad.jp>
2012-08-27 17:53:30 +0900

    Added a new class.

---
+++ BalanceGame/GameScene/BGGameBalloon.m
@@ -0,0 +1,14 @@
+//
+//  BGGameBalloon.m
+//  BalanceGame
+//
+//  Created by Akifumi on 2012/08/27.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGGameBalloon.h"
+
+
+@implementation BGGameBalloon
+
+@end
