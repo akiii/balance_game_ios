9128351bcb71724d88a01efeceb87636f4e92498
akiii <akiii@sfc.wide.ad.jp>
2012-08-27 13:07:44 +0900

    Added blocks and method.

--- BalanceGame/GameScene/BGGameMainLayer.h
+++ BalanceGame/GameScene/BGGameMainLayer.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
+#import "BGGameManager.h"
 
 @interface BGGameMainLayer : CCLayer {
     
@@ -16,5 +17,8 @@
 @property (nonatomic, copy) BOOL (^onSetLeftAreaState)(BOOL flag);
 @property (nonatomic, copy) BOOL (^isOnRightArea)();
 @property (nonatomic, copy) BOOL (^onSetRightAreaState)(BOOL flag);
+@property (nonatomic, copy) void (^onSendAcceleration)(UIAcceleration *acceleration);
+@property (nonatomic, copy) GameState (^onGetCurrentGameState)();
 - (void)getTouchWarningState:(BOOL)show;
+- (void)moveTowerWithAcceleration:(UIAcceleration *)acceleration;
 @end
