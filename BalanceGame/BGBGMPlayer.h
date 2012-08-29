f16bf1053e1cf901425839e5e0f0ac150935c925
akiii <akiii@sfc.wide.ad.jp>
2012-08-29 10:31:34 +0900

    Added a background music player class.

--- BalanceGame/BGBGMPlayer.h
+++ BalanceGame/BGBGMPlayer.h
@@ -7,7 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+#define PLAY_BGM(filename)  [[BGBGMPlayer sharedObject] playWithFileName:filename]
+#define STOP_BGM            [[BGBGMPlayer sharedObject] stop]
 
 @interface BGBGMPlayer : NSObject
-
++ (BGBGMPlayer *)sharedObject;
+- (void)playWithFileName:(NSString *)fileName;
+- (void)stop;
 @end
--- BalanceGame/BGBGMPlayer.m
+++ BalanceGame/BGBGMPlayer.m
@@ -8,6 +8,41 @@
 
 #import "BGBGMPlayer.h"
 
+static BGBGMPlayer *shared = nil;
+
+@interface BGBGMPlayer()
+@property (nonatomic, retain) AVAudioPlayer *bgm;
+@end
+
 @implementation BGBGMPlayer
+@synthesize bgm;
+
++ (BGBGMPlayer *)sharedObject{
+    if (!shared) {
+        shared = [[self alloc] init];
+    }
+    return shared;
+}
+
+- (void)playWithFileName:(NSString *)fileName{
+    [self stop];
+    NSString *fullPath = [[NSBundle mainBundle] pathForResource:fileName ofType:nil];
+    self.bgm = [[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:fullPath] error:nil] autorelease];
+    self.bgm.currentTime = 0;
+    self.bgm.numberOfLoops = -1;
+    [self.bgm play];
+}
+
+- (void)stop{
+    if (self.bgm) {
+        [self.bgm stop];
+        self.bgm = nil;
+    }
+}
+
+- (void)dealloc{
+    shared = nil;
+    [super dealloc];
+}
 
 @end
