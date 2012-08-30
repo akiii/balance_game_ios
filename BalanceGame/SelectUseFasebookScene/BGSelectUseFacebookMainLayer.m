dc873443dde6ad76c801607f651893d52b5e06a5
akiii <akiii@sfc.wide.ad.jp>
2012-08-30 17:49:59 +0900

    Modified a plist.

---
+++ BalanceGame/SelectUseFasebookScene/BGSelectUseFacebookMainLayer.m
@@ -0,0 +1,14 @@
+//
+//  BGSelectUseFacebookMainLayer.m
+//  BalanceGame
+//
+//  Created by Akifumi on 2012/08/30.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGSelectUseFacebookMainLayer.h"
+
+
+@implementation BGSelectUseFacebookMainLayer
+
+@end
