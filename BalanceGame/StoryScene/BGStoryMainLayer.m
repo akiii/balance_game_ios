2d8dd5c37e6004d8d7a961a713835baab96132f9
XXBlackieXX <blackie.stratocaster@gmail.com>
2012-08-31 17:29:55 +0900

    Modified BGMainStoryLayer

--- BalanceGame/StoryScene/BGStoryMainLayer.m
+++ BalanceGame/StoryScene/BGStoryMainLayer.m
@@ -29,24 +29,10 @@ int labelPositionY;
 
 - (void) onEnter {
     [super onEnter];
-    
-    //スキップボタン
     CGSize screenSize = [CCDirector sharedDirector].winSize;
-    CCMenuItemImage *startButton = [CCMenuItemImage itemWithNormalImage:@"start_button.png" selectedImage:@"start_button_on.png" block:^(id sender){
-            if (self.onPressedSkipButton) {
-                self.onPressedSkipButton();
-//                [self removeChild:myMask cleanup:YES];
-            }
-        }];
     
-    startButton.position = ccp(screenSize.width/2, 50);
-
-    CCMenu *menu = [CCMenu menuWithItems:startButton, nil];
-    menu.position = ccp(0, 0);
-    [self addChild:menu];
-
     // 文字を表示する
-    titleName = @"2012/03/21(水) おはようニュースABC\r\n2行目です";
+    titleName = @"むかしむかしあるところにそれはそれは\r\nファンキーなおじいさんとおばあさんが\n住んでおりました。おじいさんはザギンで\nシースーに、おばあさんは\n渋谷のクラブで踊りに行きました。\n";
     
     // 文字数が多い場合 lineBreakMode で調整 (テクスチャサイズが大きくなる為)
     /*
@@ -54,15 +40,16 @@ int labelPositionY;
      title =  [CCLabelTTF labelWithString:titleName dimensions:CGSizeMake(1200,12) alignment:CCTextAlignmentLeft lineBreakMode:CCLineBreakModeTailTruncation fontName:@"Arial" fontSize:12];
      */
     
-    scrollLabel =  [CCLabelTTF labelWithString:titleName fontName:@"Arial" fontSize:35];
-    labelPositionY = -10;
-    scrollLabel.position = ccp(0, labelPositionY);
-    scrollLabel.color = ccc3(255, 255 ,255);
-    scrollLabel.anchorPoint = ccp(0, 0);
+    scrollLabel =  [CCLabelTTF labelWithString:titleName fontName:@"Marker felt" fontSize:22];
+    labelPositionY = 10;
+    scrollLabel.anchorPoint = ccp(0.5, 1);
+    scrollLabel.position = ccp(screenSize.width/2, labelPositionY);
+    scrollLabel.color = ccc3(48, 0, 0);
+    
     [self addChild:scrollLabel];
     NSLog(@"titleLable.contentSize.height: %f", scrollLabel.contentSize.height);
 
-    /*
+/*    
     // マスクのスプライトを作成する
     CCSprite *mask = [CCSprite spriteWithFile:@"BGStoryMask.png"];        
     mask.position = ccp(0, 0);
@@ -74,24 +61,42 @@ int labelPositionY;
     myMask.anchorPoint = ccp(0, 0);
     [self addChild:myMask z:100];
     [myMask mask];
-     */
+*/     
     
     // 文字をスクロールさせる為のスケジューラの呼び出し
-    [self schedule:@selector(update:) interval:0.1];
+    [self schedule:@selector(update:) interval:0.05];
+    
+    //----スキップボタン----
+    
+    CCMenuItemImage *startButton = [CCMenuItemImage itemWithNormalImage:@"start_button.png" selectedImage:@"start_button_on.png" block:^(id sender){
+        if (self.onPressedSkipButton) {
+            self.onPressedSkipButton();
+            //                [self removeChild:myMask cleanup:YES];
+        }
+    }];
+    
+    startButton.position = ccp(screenSize.width/2, 50);
+    
+    CCMenu *menu = [CCMenu menuWithItems:startButton, nil];
+    menu.position = ccp(0, 0);
+    [self addChild:menu];
+    
+    //----スキップボタンここまで----
  
 }
 
 - (void)update:(ccTime)dt {
+    
+    CGSize screenSize = [CCDirector sharedDirector].winSize;
+    
     // 文字の位置を移動する
-        
-    labelPositionY = labelPositionY + 3;
+    labelPositionY = labelPositionY + 1.5;
     
     if(labelPositionY > 330){
-        labelPositionY = -10;
+        labelPositionY = 10;
     }
     
-//    [self.scrollLabel setPosition: ccp(0, labelPositionY)];
-    self.scrollLabel.position = ccp(0, labelPositionY);
+    self.scrollLabel.position = ccp(screenSize.width/2, labelPositionY);
 
 //    [myMask mask];
     
