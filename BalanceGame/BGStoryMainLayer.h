6a1cd3c344961236b45139cd54248b1610b50e1d
XXBlackieXX <blackie.stratocaster@gmail.com>
2012-08-31 11:27:03 +0900

    Modified BGStoryMainLayer

--- BalanceGame/BGStoryMainLayer.h
+++ BalanceGame/BGStoryMainLayer.h
@@ -9,8 +9,31 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
-@interface BGStoryMainLayer : CCLayer {
+@interface BGStoryMainLayer : CCSprite {
+    // Some screen information
+    CGSize size;
+    CGPoint screenMid;
     
+    // The given sprites
+    CCSprite *maskSprite;
+    CCSprite *objectSprite;
+    
+    // RenderTextures use for masking
+    CCRenderTexture *maskNegative;
+    CCRenderTexture *masked;
 }
+
+// Initialize a masked object based on an object sprite and a mask sprite
++ (id) createMaskForObject: (CCSprite *) object withMask: (CCSprite *) mask;
+
+// Redraw the masked image
+- (void) redrawMasked;
+
+// When dynamic masking is active, you have the ability to change the masked object or the mask itself
+- (void) setObject: (CCSprite *) object;
+- (void) setMask: (CCSprite *) mask;
+
+// Return the masked object as a sprite
+- (CCSprite *) getMaskedSprite;
 
 @end
