b84f32451b6f2c8952976dcf9b553df8a40051f2
XXBlackieXX <blackie.stratocaster@gmail.com>
2012-08-31 13:21:58 +0900

    Added CCMask class

---
+++ BalanceGame/StoryScene/CCMask.m
@@ -0,0 +1,14 @@
+//
+//  CCMask.m
+//  BalanceGame
+//
+//  Created by  on 12/08/31.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCMask.h"
+
+
+@implementation CCMask
+
+@end
